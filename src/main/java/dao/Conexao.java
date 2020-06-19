eb6cb2cb2207a619eb111b83c68e53fa549d08d4
jaimemartins <jaime.oliveira@df.estudante.senai.br>
2020-06-19 19:32:31 -0300

    Fase 5

---
+++ src/main/java/dao/Conexao.java
@@ -0,0 +1,19 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexao {
+	
+	public static Connection getConectar() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://sql10.freemysqlhosting.net/sql10349298", "sql10349298", "PvIQkavYlY");
+			return con;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} 	
+	}
+
+}
